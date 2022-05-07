e17cbd564b191743d41b759b84685212c5d29af0
Primetraineesvn <primetraineesvn@gmail.com>
2022-05-07 17:45:46 +0100

    Today's class update 06/05/2022

--- src/main/java/com/nopcommerce/demo/pages/HomePage.java
+++ src/main/java/com/nopcommerce/demo/pages/HomePage.java
@@ -1,15 +1,27 @@
 package com.nopcommerce.demo.pages;
 
 import com.nopcommerce.demo.utility.Utility;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * Created by Jay Vaghani
  */
 public class HomePage extends Utility {
 
-    By loginLink = By.linkText("Log in");
-    By registerLink = By.linkText("Register");
+    public HomePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(linkText = "Log in")
+    WebElement loginLink;
+
+    @FindBy(linkText = "Register")
+    WebElement registerLink;
+
+//    By loginLink = By.linkText("Log in");
+//    By registerLink = By.linkText("Register");
 
     public void clickOnLoginLink() {
         clickOnElement(loginLink);
--- src/test/java/com/nopcommerce/demo/testsuite/LoginPageTest.java
+++ src/test/java/com/nopcommerce/demo/testsuite/LoginPageTest.java
@@ -4,6 +4,7 @@ import com.nopcommerce.demo.pages.HomePage;
 import com.nopcommerce.demo.pages.LoginPage;
 import com.nopcommerce.demo.testbase.TestBase;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -12,8 +13,15 @@ import org.testng.annotations.Test;
 
 public class LoginPageTest extends TestBase {
 
-    HomePage homePage = new HomePage();
-    LoginPage loginPage = new LoginPage();
+    HomePage homePage;
+    LoginPage loginPage;
+
+
+    @BeforeMethod
+    public void inIt(){
+        homePage = new HomePage();
+        loginPage = new LoginPage();
+    }
 
     @Test
     public void verifyUserShouldNavigateToLoginPage() {
